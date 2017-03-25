fda387a8bc6119d6da7e5e64147146ee2f95ebc9
anunez <anunez@aluminyze.com>
2017-03-25 11:19:32 -0600

    Adding routing, navmenu and bootstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
+import { routedComponents, AppRoutingModule } from './app-routing.module';
+import { NavMenuComponent } from './home/nav-menu.component';
+import { AlertModule } from 'ng2-bootstrap';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    routedComponents
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
